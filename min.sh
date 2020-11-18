b8b14ce8b019556bed8e55f4cfadc6f35ee845fd
Paco Toledo <pacotole@crea.me>
2020-11-18 19:01:01 +0100

    Updated min.sh & minifed assets

--- min.sh
+++ min.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Supersimple minify script
+# required:
+# npm install uglify-js -g
+# npm install uglifycss -g
 
 echo "- Minify admin assets"
-curl -X POST -s --data-urlencode 'input@admin/css/joinchat.css' https://cssminifier.com/raw > admin/css/joinchat.min.css
-curl -X POST -s --data-urlencode 'input@admin/js/joinchat.js' https://javascript-minifier.com/raw > admin/js/joinchat.min.js
+uglifyjs admin/js/joinchat.js --compress --mangle -o admin/js/joinchat.min.js
+uglifycss admin/css/joinchat.css > admin/css/joinchat.min.css
 echo "- Minify public javascript"
-# curl -X POST -s --data-urlencode 'input@public/css/joinchat.css' https://cssminifier.com/raw > public/css/joinchat.min.css
-curl -X POST -s --data-urlencode 'input@public/js/joinchat.js' https://javascript-minifier.com/raw > public/js/joinchat.min.js
+uglifyjs public/js/joinchat.js --compress --mangle -o public/js/joinchat.min.js
 echo
 echo "OK"
 echo
