8ab897f7897d516d6d662d099a3c5f6c647a30fc
Paco Toledo <pacotole@crea.me>
2023-09-11 11:14:19 +0200

    minify new admin assets

--- min.sh
+++ min.sh
@@ -7,7 +7,10 @@
 
 echo "- Minify admin assets"
 uglifyjs admin/js/joinchat.js --compress --mangle -o admin/js/joinchat.min.js
+uglifyjs admin/js/joinchat-page.js --compress --mangle -o admin/js/joinchat-page.min.js
+uglifyjs admin/js/joinchat-onboard.js --compress --mangle -o admin/js/joinchat-onboard.min.js
 uglifycss admin/css/joinchat.css > admin/css/joinchat.min.css
+uglifycss admin/css/joinchat-onboard.css > admin/css/joinchat-onboard.min.css
 echo "- Minify public javascript"
 uglifyjs public/js/joinchat.js --compress --mangle -o public/js/joinchat.min.js
 uglifyjs public/js/joinchat-lite.js --compress --mangle -o public/js/joinchat-lite.min.js
