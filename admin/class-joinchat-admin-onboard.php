ec297b8c5ddc24f8bcc2b96402497a87273a5e5d
Paco Toledo <pacotole@crea.me>
2025-01-15 09:38:13 +0100

    Fix deleted $user in onboard

--- admin/class-joinchat-admin-onboard.php
+++ admin/class-joinchat-admin-onboard.php
@@ -169,6 +169,8 @@ class Joinchat_Admin_Onboard {
 			wp_enqueue_style( 'intl-tel-input' );
 		}
 
+		$user = wp_get_current_user();
+
 		$config = array(
 			'settings_url' => add_query_arg( 'onboard', 'no', Joinchat_Util::admin_url() ),
 			'img_base'     => plugins_url( 'img/', __FILE__ ),
